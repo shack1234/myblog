6fc9b7951e9a203a0e67816a2a4605bc6c1fb226
shack1234 <shadrack.anayo@strathmore.edu>
2020-05-15 13:09:27 +0300

    removed email and pass'

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export MAIL_USERNAME='maestrowebsites@gmail.com'
-export MAIL_PASSWORD='S37499441'
+export MAIL_USERNAME='@gmail.com'
+export MAIL_PASSWORD=''
 export SECRET_KEY='Flask WTF Secret Key'
 
 
